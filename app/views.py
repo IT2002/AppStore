6cc1b2612240ce3743f277d6a427a619556858ab
chenghong99 <chenghong3388@gmail.com>
2022-03-11 21:26:39 +0800

    First commit

--- app/views.py
+++ app/views.py
@@ -2,6 +2,10 @@ from django.shortcuts import render, redirect
 from django.db import connection
 
 # Create your views here.
+def say_hello(request):
+    return render(request, 'hello.html')
+
+# Create your views here.
 def index(request):
     """Shows the main page"""
 
