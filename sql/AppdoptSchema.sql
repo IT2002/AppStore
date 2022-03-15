0d0aa77af2d3f205b9e9df33e6ecac3c3974d3e6
ErSongHeng <100456048+ErSongHeng@users.noreply.github.com>
2022-03-15 23:35:34 +0800

    Commit 1
    Draft of the first Appdopt Schema

---
+++ sql/AppdoptSchema.sql
@@ -0,0 +1,30 @@
+CREATE TABLE IF NOT EXISTS users(
+	first_name VARCHAR(64) NOT NULL,
+	last_name VARCHAR(64) NOT NULL,
+	email VARCHAR(128) UNIQUE NOT NULL,
+	username VARCHAR(50) UNIQUE NOT NULL,
+	phone_number VARCHAR(16) PRIMARY KEY,
+	password VARCHAR(50) NOT NULL
+);
+	
+CREATE TABLE IF NOT EXISTS posts(
+	post_id INTEGER PRIMARY KEY,
+	username VARCHAR(50) REFERENCES users(username),
+	pet VARCHAR(20) NOT NULL,
+	breed VARCHAR(50) NOT NULL,
+	date_of_post DATE NOT NULL,
+	age_of_pet INTEGER NOT NULL,
+	price NUMERIC NOT NULL CHECK(price > 0),
+	description VARCHAR(1200) NOT NULL,
+	status VARCHAR(50) DEFAULT 'AVAILABLE',
+	phone_number VARCHAR(16),
+	FOREIGN KEY (phone_number) REFERENCES users(phone_number) ON DELETE CASCADE ON UPDATE CASCADE
+	);
+  
+CREATE TABLE transactions(
+	post_id INTEGER,
+	date_of_sale DATE NOT NULL,
+	seller_phone_number VARCHAR(16) REFERENCES users(phone_number),
+	buyer_phone_number VARCHAR(16) REFERENCES users(phone_number),
+	FOREIGN KEY (post_id) REFERENCES posts(post_id)
+);
