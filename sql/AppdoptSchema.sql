59cbefd8ffcc52d9ab9c461bf89f0380cc221184
ErSongHeng <100456048+ErSongHeng@users.noreply.github.com>
2022-03-16 15:46:09 +0800

    Update AppdaptSchema
    Changed users table PRIMARY KEY to username, changed posts table FOREIGN KEY to username, removed phone number from posts table, added gender to posts table, added PRIMARY KEY to transactions table and added a few cascade constraints

--- sql/AppdoptSchema.sql
+++ sql/AppdoptSchema.sql
@@ -2,14 +2,14 @@ CREATE TABLE IF NOT EXISTS users(
 	first_name VARCHAR(64) NOT NULL,
 	last_name VARCHAR(64) NOT NULL,
 	email VARCHAR(128) UNIQUE NOT NULL,
-	username VARCHAR(50) UNIQUE NOT NULL,
-	phone_number VARCHAR(16) PRIMARY KEY,
+	username VARCHAR(50) PRIMARY KEY,
+	phone_number VARCHAR(16) UNIQUE NOT NULL,
 	password VARCHAR(50) NOT NULL
 );
 	
 CREATE TABLE IF NOT EXISTS posts(
 	post_id INTEGER PRIMARY KEY,
-	username VARCHAR(50) REFERENCES users(username),
+	username VARCHAR(50),
 	pet VARCHAR(20) NOT NULL,
 	breed VARCHAR(64) NOT NULL,
 	date_of_post DATE NOT NULL,
@@ -18,14 +18,15 @@ CREATE TABLE IF NOT EXISTS posts(
 	description VARCHAR(1200),
 	title VARCHAR(128) NOT NULL,
 	status VARCHAR(50) DEFAULT 'AVAILABLE',
-	phone_number VARCHAR(16),
-	FOREIGN KEY (phone_number) REFERENCES users(phone_number) ON DELETE CASCADE ON UPDATE CASCADE
+	gender VARCHAR(6),
+	FOREIGN KEY (username) REFERENCES users(username) ON DELETE CASCADE ON UPDATE CASCADE
 	);
   
 CREATE TABLE transactions(
 	post_id INTEGER,
 	date_of_sale DATE NOT NULL,
-	seller_phone_number VARCHAR(16) REFERENCES users(phone_number),
-	buyer_phone_number VARCHAR(16) REFERENCES users(phone_number),
-	FOREIGN KEY (post_id) REFERENCES posts(post_id)
+	seller_phone_number VARCHAR(16) REFERENCES users(phone_number) ON DELETE CASCADE ON UPDATE CASCADE,
+	buyer_phone_number VARCHAR(16) REFERENCES users(phone_number) ON DELETE CASCADE ON UPDATE CASCADE,
+	PRIMARY KEY (post_id, seller_phone_number, buyer_phone_number),
+	FOREIGN KEY (post_id) REFERENCES posts(post_id) ON DELETE CASCADE ON UPDATE CASCADE
 );
