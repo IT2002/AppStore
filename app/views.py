c0a197a9d15f3f7c02f2c655a187ccc8b5245918
JH1816 <tanjunhengonly@gmail.com>
2022-03-18 15:44:07 +0800

    created register page

--- app/views.py
+++ app/views.py
@@ -101,3 +101,29 @@ def edit(request, id):
     context["status"] = status
  
     return render(request, "app/edit.html", context)
+
+
+def register(request):
+    context = {}
+    status = ''
+
+    if request.POST:
+        ## Check if customerid is already in the table
+        with connection.cursor() as cursor:
+
+            cursor.execute("SELECT * FROM customers WHERE customerid = %s", [request.POST['customerid']])
+            customer = cursor.fetchone()
+            ## No customer with same id
+            if customer == None:
+                ##TODO: date validation
+                cursor.execute("INSERT INTO customers VALUES (%s, %s, %s, %s, %s, %s, %s)"
+                        , [request.POST['first_name'], request.POST['last_name'], request.POST['email'],
+                           request.POST['dob'] , request.POST['since'], request.POST['customerid'], request.POST['country'] ])
+                return redirect('home')    
+            else:
+                status = 'Customer with ID %s already exists' % (request.POST['customerid'])
+
+
+    context['status'] = status
+ 
+    return render(request, "app/register.html", context)
