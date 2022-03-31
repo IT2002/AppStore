68965953a0a1eb2c267ca539896a2a04dbd161c0
hjx <52339333+itsmejx@users.noreply.github.com>
2022-03-31 19:46:38 +0800

    Create AppStoreApplications.sql

---
+++ sql/AppStoreApplications.sql
@@ -0,0 +1,9 @@
+insert into applications values
+('mlow@gmail.com', 1),
+('blee@gmail.com', 2),
+('jxhan@gmail.com', 3),
+('gmacqueen3@jalbum.net',4),
+('stasker4@goo.ne.jp', 5),
+('msenussi7@independent.co.uk', 4),
+('scoppledike8@google.com.hk', 3),
+('emcgaw9@smugmug.com', 2);
