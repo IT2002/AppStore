5e8c7570859a3a609a49b9f0e2acf9312357cccb
hjx <52339333+itsmejx@users.noreply.github.com>
2022-03-31 19:44:57 +0800

    Create AppStorePastExp.sql

---
+++ sql/AppStorePastExp.sql
@@ -0,0 +1,15 @@
+/*******************
+
+Test past_exp for 10 people
+
+*******************/
+
+INSERT INTO past_exp VALUES
+('mlow@gmail.com', 'Apple', 'Computer peripheral equipment', 'Apple Magicican', 10),
+('blee@gmail.com', 'Google', 'Pharmaceuticals', 'Lab Tech', 5),
+('jxhan@gmail.com', 'Samsung', 'Bank', 'Banker', 7),
+('gmacqueen3@jalbum.net', 'SGH', 'Nursing', 'Nursing', 20),
+('stasker4@goo.ne.jp', 'Amazon', 'Retail', 'Seller', 1),
+('msenussi7@independent.co.uk', 'Facebook', 'Marketing', 'Influencer', 3),
+('scoppledike8@google.com.hk', 'Google', 'Technology', 'Coder', 5),
+('emcgaw9@smugmug.com', 'SGH', 'Healthcare', 'Doctor', 30);
